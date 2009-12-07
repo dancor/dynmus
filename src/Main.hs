ffe0bfe8549c122431cda43e218741eeb113c7f5
Daniel Corson <danl@alum.mit.edu>
2009-12-06 18:52:06 -0800

    "just make something work first" idea for csound integration

--- src/Main.hs
+++ src/Main.hs
@@ -1,38 +1,15 @@
-{-# LANGUAGE QuasiQuotes #-}
-
 module Main where
 
-import Control.Monad.Error
-import Heredoc (here)
-import Sound.Csound
-
-csd = [$here|<CsoundSynthesizer>
-  <CsOptions>
-    csound -d
-  </CsOptions>
-  <CsInstruments>
-    sr     = 44100           ; Sample rate.
-    kr     = 4410            ; Control signal rate.
-    ksmps  = 10              ; Samples pr. control signal.
-    nchnls = 1               ; Number of output channels.
-    instr 1
-    a1     oscil p4, p5, 1   ; Simple oscillator.
-           out a1            ; Output.
-    endin
-  </CsInstruments>
-  <CsScore>
-    f1 0 8192 10 1           ; Table containing a sine wave.
-    i1 0 1 20000 1000        ; Play one second of one kHz tone.
-    e
-  </CsScore>
-</CsoundSynthesizer>|]
-
-runCsoundSimple :: String -> CsoundMonad CsoundPerformStatus
-runCsoundSimple = undefined
+import System.IO
+import System.Process
 
 main :: IO ()
 main = do
   print "hi"
-  res <- runErrorT $ runCsoundSimple csd
-  print res
+  (pIn, pOut, pErr, pId) <- runInteractiveProcess "csound"
+    ["-d", "-odac", "-L", "stdin", "hack.csd"]
+    Nothing Nothing
+  hClose pIn
+  waitForProcess pId
+  print "done"
 
