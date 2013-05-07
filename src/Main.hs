a7e56ec52731407da24a9c39a28df6536e4aedfb
Daniel Corson <danl@alum.mit.edu>
2013-05-07 16:57:30 +0800

    more documentation for the test cases

--- src/Main.hs
+++ src/Main.hs
@@ -15,6 +15,11 @@ volume :: Double
 volume = 0.8
 
 -- Yampa sine wave calling (sin).
+--
+-- Plays smoothly and correct tone.  Though the program runs in ~0.6 s
+-- instead of the anticipated ~1.0 s.  Changing the (Y.after 1) to
+-- (Y.after 2) runs in ~1.6 s.  I guess Yampa is just doing something weird
+-- that chews up the first ~0.4 s of execution.
 testSigYampa :: Y.SF () (Double, Y.Event ())
 testSigYampa =
     (Y.time >>> Y.arr (\ t -> volume * sin (2 * pi * freq * t)))
@@ -22,6 +27,9 @@ testSigYampa =
     Y.after 1 ()
 
 -- Sine wave calling (sin).
+--
+-- I get popping and maybe warbling and the pitch seems low.
+-- Program run time is correctly ~1.0 s.
 testSig :: WireP () Double
 testSig = (\ t -> volume * sin (2 * pi * freq * t)) <$> time . for 1
 
@@ -37,6 +45,9 @@ sineTable = Vec.fromList $
         [0 .. sineTableSize - 1]
 
 -- Sine wave with a sine table.
+--
+-- I get popping and maybe warbling and the pitch seems low.
+-- Program run time is correctly ~1.0 s.
 testSigTable :: WireP () Double
 testSigTable = (
     (\ t -> volume * (sineTable Vec.!
@@ -44,6 +55,9 @@ testSigTable = (
     ) <$> time) . for 1
 
 -- Sawtooth wave. 
+--
+-- I get popping and maybe warbling and the pitch seems low.
+-- Program run time is correctly ~1.0 s.
 testSigSawtooth :: WireP () Double
 testSigSawtooth = ((decPart . (* freq)) <$> time) . for 1
 
